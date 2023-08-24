a22947c2a9178eb39f44673a3874336139d6ffbd
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2023-08-24 17:49:26 -0300

    Add Section Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,73 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <section className="bg-pink-200">
+    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+      <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+        <div className="lg:col-span-2 lg:py-12">
+          <p className="max-w-xl text-lg">
+            At the same time, the fact that we are wholly owned and totally
+            independent from manufacturer and other group control gives you
+            confidence that we will only recommend what is right for you.
+          </p>
+          <div className="mt-8">
+            <a href="" className="text-2xl font-bold text-pink-600">
+              0151 475 4450
+            </a>  
+            <address className="mt-2 not-italic">
+              282 Kevin Brook, Imogeneborough, CA 58517
+            </address>
+          </div>
+        </div>
+        <div className="rounded-lg bg-red-300 p-8 shadow-lg lg:col-span-3 lg:p-12">
+          <form action="" className="space-y-4">
+            <div>              
+              <input
+                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                placeholder="Name"
+                type="text"
+                id="name"
+              />
+            </div>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+              <div>                
+                <input
+                  className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                  placeholder="Email address"
+                  type="email"
+                  id="email"
+                />
+              </div>
+              <div>
+                <input
+                  className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                  placeholder="Phone Number"
+                  type="tel"
+                  id="phone"
+                />
+              </div>
+            </div>              
+            <div>                
+              <textarea
+                className="w-full rounded-lg border-gray-00 p-3 text-sm"
+                placeholder="Message"
+                rows="8"
+                id="message"
+              ></textarea>
+            </div>  
+            <div className="mt-4">
+              <button
+                type="submit"
+                className="inline-block w-full rounded-lg bg-red-500 hover:bg-red-600 px-5 py-3 font-medium text-white sm:w-auto"
+              >
+                Send Message
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section> 
   )
 }
 
